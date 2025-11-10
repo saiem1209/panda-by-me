a2bda5588e0a9319e7f9af4aa468fc65d9d36795
saiem1209 <saiemhossain2001@gmail.com>
2025-11-10 11:25:24 +0600

    more shoes added at se all button

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const loginBtn = document.getElementById("h-login-btn");
 
-loginBtn.addEventListener('click', function(e){
-    e.preventDefault()
-    window.location.href=('./login.html');
-})
+loginBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "./login.html";
+});
 
 const registrationBtn = document.getElementById("h-registration-btn");
 
-registrationBtn.addEventListener('click', function(e){
-    e.preventDefault()
-    window.location.href=('./registration.html');
-})
+registrationBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "./registration.html";
+});
+
+function displayProducts(productsArray) {
+  const container = document.getElementById("products-container");
+  container.innerHTML = ""; 
+  productsArray.forEach((product) => {
+    const cardDiv = document.createElement("div");
+
+    cardDiv.className = "card bg-base-100 w-90 shadow-sm";
+
+    cardDiv.innerHTML = `
+        <figure>
+        <img 
+            src="${product.imageURL}" 
+            alt="${product.productName}" 
+            onerror="this.onerror=null; this.src='https://via.placeholder.com/300x200?text=Default+Shoe+Image';"
+        />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">${product.productName}</h2>
+                    <p>${product.Description}</p>
+                    <div class="card-actions justify-end items-center">
+                        <p class="font-semibold text-2xl">${product.price}</p>
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+    `;
+
+    container.appendChild(cardDiv);
+  });
+
+}
+
+const allShoes = document.getElementById("all-shoes");
+
+allShoes.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) => displayProducts(data))
+    .catch((error) => console.error("Error loading products:", error));
+});
